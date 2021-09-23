9d66df4bcebd7cd0fe0dde4ab3a424635824b270
Sp1dzAntisaus <85760686+sp1dzsaus@users.noreply.github.com>
2021-09-23 07:47:30 +0300

    Минор эдит: перераспределение классов в модуль cardgame.py

---
+++ cardgame.py
@@ -0,0 +1,34 @@
+from engine import *
+from gamelogic import *
+from cards import *
+
+class CardSprite(Sign):
+    def __init__(self, card, position=Vector2(0, 0)):
+        super().__init__(background=card.type.portrait,
+                         text='Stats here',
+                         position=position,
+                         text_shift=Vector2(50, 120),
+                         font_size=22)
+        self.dragging = False
+
+    def tick(self, dt):
+        super().tick(dt)
+        if pyglet.window.mouse_impulse:
+            if self.dragging:
+                self.dragging = False
+            elif self.pointIntersection(pyglet.window.mouse_position):
+                self.dragging = True
+        
+        if self.dragging:
+            dist = pyglet.window.mouse_position - self.center
+            self.move(*(dist / 10))
+
+class Board:
+    def __init__(self):
+        self.player_1 = RandyAI()
+        self.player_2 = RandyAI()
+
+    def random_deck(self, stock=None):
+        if stock:
+            return random.shuffle(stock)
+        ...
--- engine.py
+++ engine.py
@@ -54,26 +54,3 @@ class Sign(Sprite):
         super().render()
         self.syncLabelPosition()
         self.label.draw()
-        
-
-class CardSprite(Sign):
-    def __init__(self, card, position=Vector2(0, 0)):
-        super().__init__(background=card.type.portrait,
-                         text='Stats here',
-                         position=position,
-                         text_shift=Vector2(50, 120),
-                         font_size=22)
-        self.dragging = False
-
-    def tick(self, dt):
-        super().tick(dt)
-        if pyglet.window.mouse_impulse:
-            if self.dragging:
-                self.dragging = False
-            elif self.pointIntersection(pyglet.window.mouse_position):
-                self.dragging = True
-        
-        
-        if self.dragging:
-            dist = pyglet.window.mouse_position - self.center
-            self.move(*(dist / 10))
